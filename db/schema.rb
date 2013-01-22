ecbbb673d3f47de1370cb1968878e6bf27d36894
Josh <josh@Joshs-MacBook-Air.local>
2013-01-22 16:54:24 -0600

    add schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130122151834) do
+ActiveRecord::Schema.define(:version => 20130122205106) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -45,6 +45,18 @@ ActiveRecord::Schema.define(:version => 20130122151834) do
 
   add_index "admin_users", ["email"], :name => "index_admin_users_on_email", :unique => true
   add_index "admin_users", ["reset_password_token"], :name => "index_admin_users_on_reset_password_token", :unique => true
+
+  create_table "sessions", :force => true do |t|
+    t.datetime "start"
+    t.datetime "end"
+    t.string   "title"
+    t.text     "description"
+    t.string   "speaker"
+    t.string   "type"
+    t.string   "location"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
   create_table "speakers", :force => true do |t|
     t.string   "name"
