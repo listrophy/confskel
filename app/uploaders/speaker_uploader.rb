dcb1b1e6df7c932aad1efe378fcda44c1a1ba956
Josh Hoff <randomenduser@gmail.com>
2013-01-28 11:51:38 -0600

    Switch Carrierwave to use file storage in tests
    - Use postgres databases locally

--- app/uploaders/speaker_uploader.rb
+++ app/uploaders/speaker_uploader.rb
@@ -3,12 +3,6 @@
 class SpeakerUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
-  if Rails.env.test?
-    storage :file
-  else
-    storage :fog
-  end
-
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,15 @@
 CarrierWave.configure do |config|
   # config.permissions = 0666
   # config.directory_permissions = 0777
-  # config.storage = :file
+  #config.storage = :file
+
+  if Rails.env.test?
+    config.storage :file
+    config.enable_processing = false
+  else
+    config.storage :fog
+  end
+
   config.fog_credentials = {
     :provider               => 'AWS',
     :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
