f515c78033837bee3530528766f80a0ba854f3a8
Joshua Hoff <randomenduser@gmail.com>
2013-03-25 13:01:21 -0500

    add proposal approval feature
    it works, but needs testing

--- app/admin/proposals.rb
+++ app/admin/proposals.rb
@@ -1,5 +1,32 @@
 ActiveAdmin.register Proposal do
+
+  member_action :approve, method: :put do
+
+    proposal = Proposal.find params[:id]
+    speaker = Speaker.new name: proposal.name, twitter: proposal.twitter
+
+    if speaker.save
+      flash[:message] = """
+        Speaker \"#{speaker.name}\" has been approved.
+        You can fill in extra details here, or you can come back later;
+        everything has been saved up to this point
+      """
+      redirect_to edit_admin_speaker_path(speaker.id)
+    else
+      flash[:error] = 'Cannot approve a speaker without a name'
+      redirect_to admin_proposals_path
+    end
+
+  end
+
   index do
+    column do |proposal|
+      if Speaker.exists? name: proposal.name
+        link_to 'View speaker', admin_speaker_path(Speaker.find_by_name(proposal.name))
+      else
+        link_to 'Approve', approve_admin_proposal_path(proposal), method: :put
+      end
+    end
     column :title
     column :name
     column :email
@@ -17,4 +44,5 @@ ActiveAdmin.register Proposal do
     end
     default_actions
   end
+
 end
